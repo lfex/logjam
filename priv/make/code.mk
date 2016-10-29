37b6221f605c58a0a246d36bd615055a5b928f9c
Duncan McGreggor <oubiwann@gmail.com>
2016-10-29 13:30:33 -0500

    Added build and publish targets.

--- priv/make/code.mk
+++ priv/make/code.mk
@@ -13,7 +13,31 @@ shell:
 
 clean:
 	@rebar3 clean
-	@rm -rf ebin/* _build/default/lib/$(PROJECT)
+	@rm -rf ebin/* _build/default/lib/$(PROJECT) rebar.lock
 
 clean-all: clean
 	@rebar3 as dev lfe clean
+
+push:
+	git push github master
+	git push gitlab master
+
+push-tags:
+	git push github --tags
+	git push gitlab --tags
+
+push-all: push push-tags
+
+build-github: clean
+	rebar3 compile
+
+build-gitlab: clean
+	rebar3 as gitlab compile
+
+build-hexpm: clean
+	rebar3 as hexpm compile
+
+build-all: build-github build-gitlab build-hexpm
+
+publish: clean
+	rebar3 as hexpm hex publish
